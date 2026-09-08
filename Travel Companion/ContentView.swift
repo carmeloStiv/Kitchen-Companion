b3129550066f771980b1ccb1ac9c60ae3996bd04
Carmelo <136690391+phantomda@users.noreply.github.com>
2026-09-08 15:15:40 +1000

    Initial Commit

---
+++ Travel Companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel Companion
+//
+//  Created by Carmelo Stivala on 8/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Travel Companion/Travel_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_CompanionApp.swift
+//  Travel Companion
+//
+//  Created by Carmelo Stivala on 8/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct Travel_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
